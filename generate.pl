48431c06f65b39c6634356286f833134a1aa5e62
Zak B. Elep <zakame@zakame.net>
2026-05-15 23:58:20 +0800

    generate.pl: add alpine variant generation with Dockerfile templates

--- generate.pl
+++ generate.pl
@@ -78,10 +78,13 @@ my $config = do {
   Load <$fh>;
 };
 
-my $template = do {
+sub load_template {
+  my $family = shift;
+  open my $fh, '<', "Dockerfile.$family.template"
+    or die "Couldn't open Dockerfile.$family.template: $!";
   local $/;
-  <DATA>;
-};
+  return <$fh>;
+}
 
 my %builds;
 
@@ -127,6 +130,13 @@ if (!-d "$downloads") {
   mkdir "$downloads" or die "Couldn't create a downloads directory at $downloads";
 }
 
+my %templates = (
+  debian => load_template('debian'),
+  alpine => load_template('alpine'),
+);
+
+my $constraints = $config->{build_constraints} // {};
+
 for my $build (@{$config->{builds}}) {
   $builds{$build} = $config->{options}{common};
   $builds{"$build,threaded"} = "@{$config->{options}}{qw/threaded common/}";
@@ -139,6 +149,8 @@ for my $release (@{$config->{releases}}) {
   die "Bad version: $release->{version}" unless $release->{version} =~ /\A5\.\d+\.\d+\Z/;
 
   my $patch;
+  my $busybox_ps_patch;
+  my $perl_src_dir;
   my $tarball = CPAN::Perl::Releases::MetaCPAN::perl_tarballs($release->{version})->{'tar.gz'};
   my ($file)  = File::Basename::fileparse($tarball);
   my $url     = "https://cpan.metacpan.org/authors/id/$tarball";
@@ -150,23 +162,46 @@ for my $release (@{$config->{releases}}) {
     getstore($url, "$downloads/$file");
   }
   {
-    my $dir = "$downloads/perl-$release->{version}";
-    qx{rm -fR $dir};
-    mkdir $dir or die "Couldn't create $dir";
+    $perl_src_dir = "$downloads/perl-$release->{version}";
+    qx{rm -fR $perl_src_dir};
+    mkdir $perl_src_dir or die "Couldn't create $perl_src_dir";
     qx{
-      tar -C "$downloads" -xf $dir.tar.gz &&\
-      cd $dir &&\
+      tar -C "$downloads" -xf $perl_src_dir.tar.gz &&\
+      cd $perl_src_dir &&\
       chown -R \$(id -u):\$(id -g) . &&\
       git init &&\
       git add . &&\
       git commit -m tmp
     };
     die "Couldn't create a temp git repo for $release->{version}" if $? != 0;
-    Devel::PatchPerl->patch_source($release->{version}, $dir);
+    Devel::PatchPerl->patch_source($release->{version}, $perl_src_dir);
     $patch = qx{
-      cd $dir && git -c 'diff.mnemonicprefix=false' diff
+      cd $perl_src_dir && git -c 'diff.mnemonicprefix=false' diff
     };
     die "Couldn't create a Devel::PatchPerl patch for $release->{version}" if $? != 0;
+
+    # Generate the per-Perl-version busybox-ps test skip patch, but only for
+    # releases that actually ship an alpine variant (nothing else consumes
+    # it). Keeps debian-only releases config-only and immune to a
+    # t/op/magic.t regex miss on a Perl version that never needed this patch.
+    if (grep { $_->{family} eq 'alpine' } @{$release->{os_release} // []}) {
+      # Line numbers in t/op/magic.t drift between Perl minors so this patch
+      # must be emitted per version, parallel to DevelPatchPerl.patch above.
+      # The substitution itself is stable: replace the prctl-detecting skip
+      # with a busybox-compatible one.
+      qx{cd $perl_src_dir && git add -A && git commit -m 'after-devel-patchperl' --allow-empty};
+      qx{
+        cd $perl_src_dir && perl -pi -E '
+          s{skip "We don.+?prctl\\(\\).+?".*?;}{skip "Skip test to avoid external ps(1) dependency", 2;}
+        ' t/op/magic.t
+      };
+      $busybox_ps_patch = qx{
+        cd $perl_src_dir && git -c 'diff.mnemonicprefix=false' diff
+      };
+      die "Couldn't create busybox-ps patch for $release->{version}" if $? != 0;
+      die "busybox-ps patch for $release->{version} is empty, regex may have missed t/op/magic.t"
+        unless $busybox_ps_patch =~ /\S/;
+    }
   }
 
   for my $build (keys %builds) {
@@ -178,57 +213,107 @@ for my $release (@{$config->{releases}}) {
     }
     $release->{"cpm_dist_$_"} = $cpm{$_} for keys %cpm;
 
-    $release->{extra_flags} ||= '';
+    $release->{extra_flags} //= '';
 
     $release->{image} = $build =~ /main/ ? 'buildpack-deps' : 'debian';
 
-    for my $debian_release (@{$release->{debian_release}}) {
+    for my $os (@{$release->{os_release}}) {
+      my ($family, $tag) = @{$os}{qw(family tag)};
 
-      my $output = $template;
+      # Honor build_constraints (e.g. no slim on alpine).
+      if (exists $constraints->{$family}) {
+        (my $core = $build) =~ s/,threaded$//;
+        next unless grep { $_ eq $core } @{$constraints->{$family}};
+      }
+
+      my $output = $templates{$family}
+        or die "No template loaded for family '$family'";
       $output =~ s/\{\{$_\}\}/$release->{$_}/mg for keys %$release;
       $output =~ s/\{\{args\}\}/$builds{$build}/mg;
 
-      if ($build =~ /slim/) {
-        $output =~ s/\{\{docker_slim_run_install\}\}/$docker_slim_run_install/mg;
-        $output =~ s/\{\{docker_slim_run_purge\}\}/$docker_slim_run_purge/mg;
-        $output =~ s/\{\{tag\}\}/$debian_release-slim/mg;
+      if ($family eq 'debian') {
+        if ($build =~ /slim/) {
+          $output =~ s/\{\{docker_slim_run_install\}\}/$docker_slim_run_install/mg;
+          $output =~ s/\{\{docker_slim_run_purge\}\}/$docker_slim_run_purge/mg;
+          $output =~ s/\{\{tag\}\}/$tag-slim/mg;
+        }
+        else {
+          $output =~ s/\{\{docker_slim_run_install\}\}/true/mg;
+          $output =~ s/\{\{docker_slim_run_purge\}\}/true/mg;
+          $output =~ s/\{\{tag\}\}/$tag/mg;
+        }
+      }
+      elsif ($family eq 'alpine') {
+        (my $alpine_tag_short = $tag) =~ s/^alpine//;
+        $output =~ s/\{\{alpine_tag_short\}\}/$alpine_tag_short/mg;
       }
       else {
-        $output =~ s/\{\{docker_slim_run_install\}\}/true/mg;
-        $output =~ s/\{\{docker_slim_run_purge\}\}/true/mg;
-        $output =~ s/\{\{tag\}\}/$debian_release/mg;
+        die "Unknown family '$family' for $release->{version}/$tag";
       }
 
-      my $dir = sprintf "%i.%03i.%03i-%s-%s", ($release->{version} =~ /(\d+)\.(\d+)\.(\d+)/), $build, $debian_release;
+      my $dir = sprintf "%i.%03i.%03i-%s-%s",
+        ($release->{version} =~ /(\d+)\.(\d+)\.(\d+)/), $build, $tag;
 
       mkdir $dir unless -d $dir;
 
-      # Set up the generated DevelPatchPerl.patch
+      # Set up the generated DevelPatchPerl.patch (shared across families).
       if ($patch) {
-        open my $fh, ">", "$dir/DevelPatchPerl.patch";
+        open my $fh, ">", "$dir/DevelPatchPerl.patch" or die "Couldn't write $dir/DevelPatchPerl.patch: $!";
         print $fh $patch;
+        close $fh;
         $output =~ s!\{\{docker_copy_perl_patch\}\}!COPY *.patch /usr/src/perl/!mg;
       }
       else {
         $output =~ s!\{\{docker_copy_perl_patch\}\}!# No DevelPatchPerl.patch generated!mg;
       }
 
+      # Alpine-specific patches: musl-stack-size (static) + skip-test-due-to-busybox-ps (per-version).
+      if ($family eq 'alpine') {
+        # Copy the static musl-stack-size.patch into the dir.
+        qx{cp musl-stack-size.patch "$dir/musl-stack-size.patch"};
+        die "Couldn't copy musl-stack-size.patch to $dir" if $? != 0;
+
+        # Write the per-version busybox-ps patch.
+        open my $fh, ">", "$dir/skip-test-due-to-busybox-ps.patch"
+          or die "Couldn't write $dir/skip-test-due-to-busybox-ps.patch: $!";
+        print $fh $busybox_ps_patch;
+        close $fh;
+
+        # Ensure the COPY *.patch line is enabled even if DevelPatchPerl.patch
+        # was empty for this version — musl patches need to be applied.
+        $output =~ s!^# No DevelPatchPerl.patch generated$!COPY *.patch /usr/src/perl/!mg;
+      }
+
       $release->{run_tests} //= "parallel";
-      if ($release->{run_tests} eq "serial") {
-        $output =~ s/\{\{test\}\}/make test_harness/;
-      }
-      elsif ($release->{run_tests} eq "parallel") {
-        $output =~ s/\{\{test\}\}/TEST_JOBS=\$(nproc) make test_harness/;
-      }
-      elsif ($release->{run_tests} eq "no") {
-
-        # https://metacpan.org/pod/Devel::PatchPerl#CAVEAT
-        $output =~ s/\{\{test\}\}/LD_LIBRARY_PATH=. .\/perl -Ilib -de0/;
-
-        # https://metacpan.org/pod/distribution/perl/INSTALL#Building-a-shared-Perl-library
+      if ($family eq 'alpine') {
+        # Alpine uses `test_harness_notty` per Alpine APKBUILD convention for
+        # no-tty Docker builds. Same parallel/serial/no semantics otherwise.
+        if ($release->{run_tests} eq "serial") {
+          $output =~ s/\{\{test_alpine\}\}/make test_harness_notty/;
+        }
+        elsif ($release->{run_tests} eq "parallel") {
+          $output =~ s/\{\{test_alpine\}\}/TEST_JOBS=\$(nproc) make test_harness_notty/;
+        }
+        elsif ($release->{run_tests} eq "no") {
+          $output =~ s/\{\{test_alpine\}\}/LD_LIBRARY_PATH=. .\/perl -Ilib -de0/;
+        }
+        else {
+          die "run_tests was provided for $release->{version} but is invalid; should be 'parallel', 'serial', or 'no'\n";
+        }
       }
       else {
-        die "run_tests was provided for $release->{version} but is invalid; should be 'parallel', 'serial', or 'no'\n";
+        if ($release->{run_tests} eq "serial") {
+          $output =~ s/\{\{test\}\}/make test_harness/;
+        }
+        elsif ($release->{run_tests} eq "parallel") {
+          $output =~ s/\{\{test\}\}/TEST_JOBS=\$(nproc) make test_harness/;
+        }
+        elsif ($release->{run_tests} eq "no") {
+          $output =~ s/\{\{test\}\}/LD_LIBRARY_PATH=. .\/perl -Ilib -de0/;
+        }
+        else {
+          die "run_tests was provided for $release->{version} but is invalid; should be 'parallel', 'serial', or 'no'\n";
+        }
       }
 
       open my $dockerfile, ">", "$dir/Dockerfile" or die "Couldn't open $dir/Dockerfile for writing";
@@ -252,8 +337,36 @@ generate.pl - generate Dockerfiles for Perl
 =head1 DESCRIPTION
 
 generate.pl is meant to be run from the actual repo directory, with a
-config.yml file correctly configured.  It contains with a 'releases'
-key, which contains a list of releases, each with the following keys:
+config.yml file correctly configured.
+
+The top-level config keys are:
+
+=over 4
+
+=item builds
+
+A list of build variant names (e.g. C<main>, C<slim>). Each build is
+also emitted in a threaded variant (C<main,threaded>).
+
+=item options
+
+A hash with C<common> and C<threaded> keys supplying C<Configure> flags
+shared across all releases.
+
+=item build_constraints
+
+An optional map from OS family name to an allowlist of build variants.
+When a family appears here, only the listed (non-threaded) builds are
+emitted for that family; the threaded counterparts follow automatically.
+If a family is absent, all builds apply. An empty list skips the family
+entirely.
+
+    build_constraints:
+      alpine: [main]   # no slim on alpine
+
+=back
+
+The C<releases> key contains a list of releases, each with the following keys:
 
 =over 4
 
@@ -275,20 +388,23 @@ The SHA-256 of the tarball for that release.
 
 =over 4
 
-=item debian_release
+=item os_release
 
-The Docker image tag which this Perl would build on, common to both the
-L<buildpack-deps|https://hub.docker.com/_/buildpack-deps> and
-L<debian|https://hub.docker.com/_/debian> Docker images.
+A list of OS targets this Perl release should be built for. Each entry
+is a hash with C<family> and C<tag> keys:
 
-This should be a list of tags for different Debian versions:
+    - version: 5.42.2
+      sha256: ...
+      os_release:
+        - { family: debian, tag: bullseye }
+        - { family: debian, tag: bookworm }
+        - { family: alpine, tag: alpine3.23 }
+        - { family: alpine, tag: alpine3.24 }
 
-    - version: 5.30.0
-      debian_release:
-        - bullseye
-        - buster
-
-C<-slim> will be appended to this value for C<slim> builds.
+C<family> is either C<debian> or C<alpine>. C<tag> is the OS-specific
+release tag. For Debian families, C<-slim> is appended to the tag for
+C<slim> builds. For Alpine families, only the builds listed in
+C<build_constraints> (top-level key) are emitted.
 
 =item extra_flags
 
@@ -305,54 +421,53 @@ Added due to dist/IO/t/io_unix.t failing when TEST_JOBS > 1, but should
 only be used in case of a documented issue or old release (see
 L<Devel::PatchPerl's CAVEAT|https://metacpan.org/pod/Devel::PatchPerl#CAVEAT>).
 
-Default: C<yes>
+Default: C<parallel>
 
 =back
+
+=back
+
+=head1 ALPINE VARIANT GENERATION
+
+For each release with C<family: alpine> entries in C<os_release>,
+C<generate.pl> performs additional steps:
+
+=over 4
+
+=item Template dispatch
+
+Alpine Dockerfiles are rendered from C<Dockerfile.alpine.template>
+instead of C<Dockerfile.debian.template>. The C<{{alpine_tag_short}}>
+template variable is derived by stripping the C<alpine> prefix from the
+OS tag (e.g. C<alpine3.23> → C<3.23>).
+
+=item musl-stack-size.patch
+
+The static C<musl-stack-size.patch> (in the repo root) is copied into
+every alpine output directory. It bumps the minimum thread stack size to
+256k, working around L<perl5#18160|https://github.com/Perl/perl5/issues/18160>
+which remains open on musl/Alpine.
+
+=item skip-test-due-to-busybox-ps.patch
+
+A per-Perl-version patch is generated by editing C<t/op/magic.t> to
+replace the C<prctl()>-detecting ps-skip with a busybox-compatible one.
+Because the relevant line numbers in C<t/op/magic.t> drift between Perl
+minors, this patch must be regenerated for each version rather than
+being static.
+
+=item C<COPY *.patch> always enabled
+
+For alpine directories, the C<COPY *.patch> Dockerfile line is
+force-enabled even when C<DevelPatchPerl.patch> is empty, so that the
+musl-specific patches are always applied.
+
+=item Test harness
+
+Alpine builds use C<test_harness_notty> instead of C<test_harness> (the
+C<{{test_alpine}}> template variable) per Alpine APKBUILD convention for
+no-tty Docker environments.
 
 =back
 
 =cut
-
-__DATA__
-FROM {{image}}:{{tag}}
-
-{{docker_copy_perl_patch}}
-WORKDIR /usr/src/perl
-
-RUN {{docker_slim_run_install}} \
-    && curl -fL {{url}} -o perl-{{version}}.tar.gz \
-    && echo '{{sha256}} *perl-{{version}}.tar.gz' | sha256sum --strict --check - \
-    && tar --strip-components=1 -xaf perl-{{version}}.tar.gz -C /usr/src/perl \
-    && rm perl-{{version}}.tar.gz \
-    && cat *.patch | patch -p1 \
-    && gnuArch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)" \
-    && archBits="$(dpkg-architecture --query DEB_BUILD_ARCH_BITS)" \
-    && archFlag="$([ "$archBits" = '64' ] && echo '-Duse64bitall' || echo '-Duse64bitint')" \
-    && ./Configure -Darchname="$gnuArch" "$archFlag" {{args}} {{extra_flags}} -des \
-    && make -j$(nproc) \
-    && {{test}} \
-    && make install \
-    && cd /usr/src \
-    && curl -fLO {{cpanm_dist_url}} \
-    && echo '{{cpanm_dist_sha256}} *{{cpanm_dist_name}}.tar.gz' | sha256sum --strict --check - \
-    && tar -xzf {{cpanm_dist_name}}.tar.gz && cd {{cpanm_dist_name}} \
-    && {{cpanm_dist_patch_https}} \
-    && {{cpanm_dist_patch_nolwp}} \
-    && perl bin/cpanm . && cd /root \
-    && curl -fLO '{{netssleay_dist_url}}' \
-    && echo '{{netssleay_dist_sha256}} *{{netssleay_dist_name}}.tar.gz' | sha256sum --strict --check - \
-    && cpanm --notest --from $PWD {{netssleay_dist_name}}.tar.gz \
-    && curl -fLO '{{iosocketssl_dist_url}}' \
-    && echo '{{iosocketssl_dist_sha256}} *{{iosocketssl_dist_name}}.tar.gz' | sha256sum --strict --check - \
-    && SSL_CERT_DIR=/etc/ssl/certs cpanm --from $PWD {{iosocketssl_dist_name}}.tar.gz \
-    && curl -fL {{cpm_dist_url}} -o /usr/local/bin/cpm \
-    # sha256 checksum is from docker-perl team, cf https://github.com/docker-library/official-images/pull/12612#issuecomment-1158288299
-    && echo '{{cpm_dist_sha256}} */usr/local/bin/cpm' | sha256sum --strict --check - \
-    && chmod +x /usr/local/bin/cpm \
-    && {{docker_slim_run_purge}} \
-    && rm -fr /root/.cpanm /root/{{netssleay_dist_name}}* /root/{{iosocketssl_dist_name}}* /usr/src/perl /usr/src/{{cpanm_dist_name}}* /tmp/* \
-    && cpanm --version && cpm --version
-
-WORKDIR /usr/src/app
-
-CMD ["perl{{version}}","-de0"]
